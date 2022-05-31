7f060180a74019c4a6edb5efc78171caa88f5050
RuiOkazaki <okazaki200226@gmail.com>
2022-05-31 17:25:51 +0900

    update: windows用のキーバインドを追加した

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 window.addEventListener('keydown', (e) => {
-  if (e.metaKey) {
-    if (e.key === 'Enter') {
-      const username = window.location.pathname.split('/')[1];
-      window.location.href = `https://scrapbox.io/${username}/new`;
+  const userAgent = window.navigator.userAgent.toLowerCase();
+  if (userAgent.indexOf('mac os x') !== -1) {
+    // mac os 用のキーバインド
+    if (e.metaKey) {
+      if (e.key === 'Enter') {
+        const username = window.location.pathname.split('/')[1];
+        window.location.href = `https://scrapbox.io/${username}/new`;
+      }
+    }
+  } else {
+    // windows os 用のキーバインド
+    if (e.ctrlKey) {
+      if (e.key === 'Enter') {
+        const username = window.location.pathname.split('/')[1];
+        window.location.href = `https://scrapbox.io/${username}/new`;
+      }
     }
   }
 });
